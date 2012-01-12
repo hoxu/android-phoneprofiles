e9a03bfd8ee940f8933e12a27e8fd9db53557b66
Heikki Hokkanen <hoxu@users.sf.net>
2012-01-12 18:05:45 +0200

    Show profile list.

--- src/org/fealdia/android/phoneprofiles/PhoneProfilesActivity.java
+++ src/org/fealdia/android/phoneprofiles/PhoneProfilesActivity.java
@@ -1,20 +1,45 @@
 package org.fealdia.android.phoneprofiles;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PhoneProfilesActivity extends Activity {
+public class PhoneProfilesActivity extends ListActivity {
+	private int MAIN_NOTIFICATION_ID = 1;
+	private String currentProfile = null;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		showNotification();
-		setContentView(R.layout.main);
+
+		String[] PROFILES = { "General", "Meeting", "Night", "Silent" };
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.main, PROFILES));
+
+		ListView lv = getListView();
+		lv.setTextFilterEnabled(true);
+
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				// When clicked, show a toast with the TextView text
+				CharSequence choice = ((TextView) view).getText();
+				Toast.makeText(getApplicationContext(), choice, Toast.LENGTH_SHORT).show();
+				currentProfile = choice.toString();
+				showNotification();
+			}
+		});
 	}
 
 	private void showNotification() {
@@ -26,7 +51,7 @@ public class PhoneProfilesActivity extends Activity {
 		long when = System.currentTimeMillis(); // notification time
 		Context context = getApplicationContext(); // application Context
 		CharSequence contentTitle = "Phone Profiles"; // message title
-		CharSequence contentText = "Active profile: ..."; // message text
+		CharSequence contentText = "Active profile: " + currentProfile; // message text
 
 		Intent notificationIntent = new Intent(this, PhoneProfilesActivity.class);
 		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
@@ -37,6 +62,6 @@ public class PhoneProfilesActivity extends Activity {
 		notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
 		notification.flags |= Notification.FLAG_ONGOING_EVENT;
 
-		mNotificationManager.notify(1, notification);
+		mNotificationManager.notify(MAIN_NOTIFICATION_ID, notification);
 	}
 }
